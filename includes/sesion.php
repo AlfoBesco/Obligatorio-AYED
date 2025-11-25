b6d3565c5f8660307d4cf8e0936043c182d0fbad
Paola Díaz <paolamdiazp@gmail.com>
2025-11-25 20:07:30 -0300

    Creación de sesión

---
+++ includes/sesion.php
@@ -0,0 +1,28 @@
+<?php
+// require_once __DIR__ . '/../clases/Cliente.php';
+
+// session_start();
+
+// if (!isset($_SESSION['clientes'])) {
+//     $_SESSION['clientes'] = [];
+//     $_SESSION['ultimo_id'] = 0;
+    
+//     // Cargo datos de ejemplo
+//     $cliente1 = new Cliente(1, "Juan", "Pérez", "juan.perez@email.com", "099123456", "Av. 18 de Julio 1234");
+//     $cliente2 = new Cliente(2, "María", "González", "maria.gonzalez@email.com", "098765432", "Bulevar Artigas 5678");
+//     $cliente3 = new Cliente(3, "Carlos", "Rodríguez", "carlos.rodriguez@email.com", "091234567", "21 de Setiembre 9876");
+    
+//     $_SESSION['clientes'][1] = $cliente1;
+//     $_SESSION['clientes'][2] = $cliente2;
+//     $_SESSION['clientes'][3] = $cliente3;
+//     $_SESSION['ultimo_id'] = 1;
+// }
+
+// function obtenerClientes() {
+//     return $_SESSION['clientes'];
+// }
+
+// function obtenerClientePorId($id) {
+//     return isset($_SESSION['clientes'][$id]) ? $_SESSION['clientes'][$id] : null;
+// }
+?>
