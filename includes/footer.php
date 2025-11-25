d8ba119aa540c6aa32361da52bdb5ddbdb7bbc70
Paola Díaz <paolamdiazp@gmail.com>
2025-11-25 20:06:23 -0300

    Creación de footer

---
+++ includes/footer.php
@@ -0,0 +1,11 @@
+<!-- FOOTER -->
+            <!-- <div class="info-box">
+                <h3>Información del Sistema</h3>
+                <ul>
+                    <li><strong>CTC</strong> Algoritmos y estructuras de datos</li>
+                </ul>
+            </div> -->
+        </div>
+    </div>
+</body>
+</html>
